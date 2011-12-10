48e3fc44b84a929ed3bd026ca5988a82e6b32b2a
Christian Simon <cs@f03.eu>
2011-12-10 15:49:20 +0100

    Deprecation Warinng fix

--- source/build_prefixlist_from_rpki.py
+++ source/build_prefixlist_from_rpki.py
@@ -2,7 +2,7 @@ import pyrpki
 import socket
 
 # Fetch Authenticated ROAs and build prefix list...
-tree=pyrpki.get_roa_tree(dir='/var/rcynic/data/authenticated/')
+tree=pyrpki.get_roa_tree(dir='/home/christian/authenticated/')
 
 print tree[socket.AF_INET].show_filter_rules()
 print tree[socket.AF_INET6].show_filter_rules()
